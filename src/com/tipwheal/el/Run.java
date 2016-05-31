99b3184d18d837bbf065abeb0b8f9268308e79e2
luna <luna.lee.chiang@gmail.com>
2016-06-01 01:00:13 +0800

    -

--- src/com/tipwheal/el/Run.java
+++ src/com/tipwheal/el/Run.java
@@ -1,8 +1,6 @@
 package com.tipwheal.el;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 /**
  * Created by Ariana on 2016/5/31.
@@ -17,8 +15,17 @@ public class Run {
     static double MemAvd;
     static double Dgr;
     static double AtkNext;
-    public static void main(String[]args) throws IOException {
 
+    public static void main(String[]args) throws IOException {
+        StringBuilder sb=new StringBuilder();
+        StringBuilder sb2;
+        String information;
+        String str,str2;
+        String[]scores;
+        String A,B;
+        String[] d,g;
+        File f=new File("result.txt");
+        int scoreA,scoreB,got;
        for(OcpEnemy=0;OcpEnemy<4;OcpEnemy+=0.4){
            for(OcpBlank=0;OcpBlank<2;OcpBlank+=0.2){
                for(AtkEnemy=0;AtkEnemy<20;AtkEnemy+=2){
@@ -28,6 +35,40 @@ public class Run {
                                for(Dgr=-1;Dgr<1;Dgr+=0.1){
                                    for(AtkNext=0;AtkNext<12;AtkNext+=1.2){
                                        Process process = Runtime.getRuntime().exec("./testmach ");
+                                       try {
+                                           BufferedReader    bre = new BufferedReader(new FileReader("log.txt"));//此时获取到的bre就是整个文件的缓存流
+                                           while (( str = bre.readLine())!= null) // 判断最后一行不存在，为空结束循环
+                                           {
+                                               sb.append(str);
+                                           }
+                                           information="OcpEnemy is "+OcpEnemy+"/ "+" OcpBlank is "+OcpBlank+" /"+" AtkEnemy is "+AtkEnemy+"/ "+
+                                                   " Hide is "+Hide+"/ "+" OutHome is "+OutHome+"/ "+" MemAvd is "+MemAvd+" "+" Dgr is "+Dgr +
+                                                   " "+" AtkNext is "+AtkNext+"/ ";
+                                           str2=sb.toString();
+                                           scores=str2.split("#");
+                                           A=scores[scores.length-2];
+                                           d=A.split(" ");
+                                           B=scores[scores.length-1];
+                                           g=B.split(" ");
+                                           scoreA=Integer.parseInt(d[2]);
+                                           scoreB=Integer.parseInt(g[2]);
+                                           got=scoreB-scoreA;
+                                           sb2 = new StringBuilder(information);
+                                           sb2.append(A);
+                                           sb2.append(B);
+
+                                           sb2.append(" the difference in value is ").append(got);
+                                           sb2.append("\n");
+                                           information=sb2.toString();
+
+                                           FileWriter fw = new FileWriter(f, true);
+                                           fw.write(information);
+                                           fw.flush();
+                                           fw.close();
+                                           bre.close();
+                                       } catch (IOException e) {
+                                           e.printStackTrace();
+                                       }
                                    }
                                }
                            }
